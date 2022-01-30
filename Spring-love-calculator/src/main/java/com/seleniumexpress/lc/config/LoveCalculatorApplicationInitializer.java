29b8b554eac6bed6d8b36eacd520afae5e20f741
Chima-Anason <tblynxx@gmail.com>
2022-01-30 13:41:37 +0800

    STEP 3: Setting Up Dispatcher Servlet

--- Spring-love-calculator/src/main/java/com/seleniumexpress/lc/config/LoveCalculatorApplicationInitializer.java
+++ Spring-love-calculator/src/main/java/com/seleniumexpress/lc/config/LoveCalculatorApplicationInitializer.java
@@ -4,12 +4,23 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.XmlWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class LoveCalculatorApplicationInitializer implements WebApplicationInitializer {
 
 	@Override
 	public void onStartup(ServletContext servletContext) throws ServletException {
-		System.out.println("my custom class onStartUp method called.....Chima");
+		
+		XmlWebApplicationContext webApplicationContext = new XmlWebApplicationContext();
+		webApplicationContext.setConfigLocation("classpath:application-config.xml");
+		
+		//create a dispatcher servlet object
+		
+		DispatcherServlet dispatcherServlet = new DispatcherServlet(webApplicationContext);
+		
+		//Register dispatcher servlet with servlet context
+		servletContext.addServlet("myDispatcherServlet", dispatcherServlet);
 
 	}
 
