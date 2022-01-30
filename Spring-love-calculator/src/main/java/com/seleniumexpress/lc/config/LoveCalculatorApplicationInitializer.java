fdcd4140969d99abe1005432380bd6241c5a9604
Chima-Anason <tblynxx@gmail.com>
2022-01-30 14:30:01 +0800

    STEP 8: Register the App Config class with Dispatcher Servlet

--- Spring-love-calculator/src/main/java/com/seleniumexpress/lc/config/LoveCalculatorApplicationInitializer.java
+++ Spring-love-calculator/src/main/java/com/seleniumexpress/lc/config/LoveCalculatorApplicationInitializer.java
@@ -4,7 +4,9 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.XmlWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
@@ -13,8 +15,8 @@ public class LoveCalculatorApplicationInitializer implements WebApplicationIniti
 	@Override
 	public void onStartup(ServletContext servletContext) throws ServletException {
 		
-		XmlWebApplicationContext webApplicationContext = new XmlWebApplicationContext();
-		webApplicationContext.setConfigLocation("classpath:application-config.xml");
+		AnnotationConfigWebApplicationContext webApplicationContext = new AnnotationConfigWebApplicationContext();
+		webApplicationContext.register(LoveCalculatorAppConfig.class);
 		
 		//create a dispatcher servlet object
 		
