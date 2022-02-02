27193864df2e824df91be4d49eca000f0d79963f
Chima-Anason <tblynxx@gmail.com>
2022-02-03 07:05:40 +0800

    Shortcut of Step 3-adjusting the controller-Using @ModelAttribute

--- Spring-love-calculator/src/main/java/com/seleniumexpress/lc/controllers/LCAppController.java
+++ Spring-love-calculator/src/main/java/com/seleniumexpress/lc/controllers/LCAppController.java
@@ -1,7 +1,7 @@
 package com.seleniumexpress.lc.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.seleniumexpress.lc.api.UserInfoDTO;
@@ -9,31 +9,24 @@ import com.seleniumexpress.lc.api.UserInfoDTO;
 @Controller
 public class LCAppController {
 
+	/*
+	 * STEP 3: Using Spring MVC Form Tag
+	 * SHORTCUT -: Introducing @ModelAttribute
+	 */
 	@RequestMapping("/")
-	public String showHomePage(Model model) {
+	public String showHomePage(@ModelAttribute("userInfo") UserInfoDTO userInfodto) {
 		
 		//Reading the default value of properties by fetching from DTO(display default-value on home-page when loaded)
-		UserInfoDTO userInfo = new UserInfoDTO();
-		model.addAttribute("userInfo", userInfo);
-		
 		
 		return "home-page";
 	}
 	
 
-	/*
-	 * STEP 2: Using DTO and Creating DTO object
-	 * use the captured object to call each property at the view page
-	 * after inputting data on the Form the spring internally calls the getter method
-	 */
+
 	@RequestMapping("/process-homepage")
-	private String showResultPage(UserInfoDTO userInfoDTO, Model model) {
+	private String showResultPage(@ModelAttribute("userInfoDTO") UserInfoDTO userInfoDTO) {
 		
 		//Writing the values to the properties by fetching from the URL
-		System.out.println("user name Name is : "+userInfoDTO.getUserName());
-		System.out.println("crush name Name is : "+userInfoDTO.getCrushName());
-		
-		model.addAttribute("userInfoDTO", userInfoDTO);
 		
 		return "result-page";
 		
