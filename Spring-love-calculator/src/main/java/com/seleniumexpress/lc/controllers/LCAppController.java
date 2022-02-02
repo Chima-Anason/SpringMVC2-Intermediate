50bb93c4fe2421a569d9b54716986df908542463
Chima-Anason <tblynxx@gmail.com>
2022-02-03 05:36:12 +0800

    STEP 1:Data-Binding Using @RequestParam-(WITHOUT BRACKET)

--- Spring-love-calculator/src/main/java/com/seleniumexpress/lc/controllers/LCAppController.java
+++ Spring-love-calculator/src/main/java/com/seleniumexpress/lc/controllers/LCAppController.java
@@ -2,6 +2,7 @@ package com.seleniumexpress.lc.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LCAppController {
@@ -15,8 +16,10 @@ public class LCAppController {
 	
 	
 	@RequestMapping("/process-homepage")
-	private String showResultPage() {
+	private String showResultPage(@RequestParam String userName,@RequestParam String crushName) {
 		
+		System.out.println("user name Name is : "+userName);
+		System.out.println("crush name Name is : "+crushName);
 		return "result-page";
 		
 	}
