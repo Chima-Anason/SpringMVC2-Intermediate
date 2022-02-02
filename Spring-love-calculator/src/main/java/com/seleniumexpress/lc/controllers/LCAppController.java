d0b806cf1ff01622569a4a6dfbd309481f27cfe8
Chima-Anason <tblynxx@gmail.com>
2022-02-03 05:51:54 +0800

    Introducing Model interface to display the Bind-Data to the result View page

--- Spring-love-calculator/src/main/java/com/seleniumexpress/lc/controllers/LCAppController.java
+++ Spring-love-calculator/src/main/java/com/seleniumexpress/lc/controllers/LCAppController.java
@@ -1,6 +1,7 @@
 package com.seleniumexpress.lc.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -16,10 +17,13 @@ public class LCAppController {
 	
 	//NB:The @RequestParam using bracket u can put the textbox name in bracket and give it a diff variable name
 	@RequestMapping("/process-homepage")
-	private String showResultPage(@RequestParam("userName") String userName1,@RequestParam("crushName") String crushName1) {
+	private String showResultPage(@RequestParam("userName") String userName1,@RequestParam("crushName") String crushName1, Model model) {
 		
 		System.out.println("user name Name is : "+userName1);
 		System.out.println("crush name Name is : "+crushName1);
+		
+		model.addAttribute("userName", userName1);
+		model.addAttribute("crushName", crushName1);
 		return "result-page";
 		
 	}
--- Spring-love-calculator/src/main/webapp/WEB-INF/view/result-page.jsp
+++ Spring-love-calculator/src/main/webapp/WEB-INF/view/result-page.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,7 +10,9 @@
 
 	<h1 align="center">Love Calculator</h1>
 	<hr>
+
+	<p>User Name is : ${userName}</p>
+	<p>Crush Name is : ${crushName}</p>
 	
-	<p>so i am getting my result page</p>
 </body>
 </html>
