495b8876cda721eb722c1d3d84a81e4a8d1e0de3
Chima-Anason <tblynxx@gmail.com>
2022-05-01 12:41:48 +0800

    Introduction to @SessionAttributes

---
+++ Spring-Session/src/main/java/com/seleniumexpress/lc/controllers/firstController.java
@@ -0,0 +1,56 @@
+package com.seleniumexpress.lc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@Controller
+@SessionAttributes({"firstName","lastName"})
+public class firstController {
+	
+	@RequestMapping("/first")
+	public String handlermethod1(Model model) {
+		
+		model.addAttribute("firstName", "Chima");
+		model.addAttribute("lastName", "John");
+		
+		model.addAttribute("next", "/second");
+		
+		return "index";
+	}
+	
+	
+	@RequestMapping("/second")
+	public String handlermethod2(Model model1) {
+		
+		String firstname = (String)model1.getAttribute("firstName");
+		System.out.println(firstname);
+		
+		String lastname = (String)model1.getAttribute("lastName");
+		System.out.println(lastname);
+		
+		//model1.addAttribute("firstName", firstname);
+		//model1.addAttribute("lastName", lastname);
+		
+		
+		model1.addAttribute("next", "/third");
+		
+		return "index";
+	}
+	
+	
+	@RequestMapping("/third")
+	public String handlermethod3(Model model) {
+		
+		
+		String lastname = (String)model.getAttribute("lastName");
+		System.out.println(lastname);
+		
+		
+		model.addAttribute("lastName", lastname);
+		
+		return "index";
+	}
+
+}
---
+++ Spring-Session/src/main/webapp/WEB-INF/view/index.jsp
@@ -0,0 +1,7 @@
+<h1 align="center">Session attributes examples</h1>
+
+First Name : ${firstName} </br>
+Last Name : ${lastName}
+<hr/>
+
+<a href="/Spring-Session/${next}">Next ${next}</a>
