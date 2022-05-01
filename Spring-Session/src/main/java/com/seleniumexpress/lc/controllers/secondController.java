13dc5ef7b99c26aa149c3b8798ef18f63cb83fd1
Chima-Anason <tblynxx@gmail.com>
2022-05-01 13:10:48 +0800

    Using @SessionAttribute in the handler method to fetch the object from session of a different controller

--- Spring-Session/src/main/java/com/seleniumexpress/lc/controllers/secondController.java
+++ Spring-Session/src/main/java/com/seleniumexpress/lc/controllers/secondController.java
@@ -3,15 +3,18 @@ package com.seleniumexpress.lc.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
 public class secondController {
 	
 	@RequestMapping("/normalMethod")
-	public String handlermethod1(Model model) {
+	public String handlermethod1(@SessionAttribute("firstName") String firstName,Model model) {
 		
-		String firstname = (String)model.getAttribute("firstName");
-		System.out.println("inside new controller method, Printing firstName"+firstname);
+		//String firstname = (String)model.getAttribute("firstName");
+		 String newFirstName = "Mr " + firstName;
+		 model.addAttribute("firstName", newFirstName);
+		System.out.println("inside new controller method, Printing firstName"+newFirstName);
 		
 		return "index";
 	}
